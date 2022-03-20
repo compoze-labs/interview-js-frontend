1ae28c0c463facbf56beb64862be24c3f6ed8947
adameichelkraut <adam.eichelkraut@compozelabs.com>
2022-03-20 10:56:26 -0500

    use styled components for the app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import React from "react"
+import styled from "styled-components"
 import FeedbackForm from "./features/feedback/FeedbackForm"
 
-const appStyle = {
-    textAlign: "center",
-    background: "linear-gradient(to right top, #FE7A2C, #FE7A2C, #FDE25E)",
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: "calc(10px + 2vmin)",
-    color: "white",
-}
+const AppContainer = styled.div`
+    text-align: center;
+    background: linear-gradient(to right top, #FE7A2C, #FE7A2C, #FDE25E);
+    min-height: 100vh;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    overflow: hidden;
+`
+
+const CenteredAppWithGutters = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center,
+`
 
 function App() {
     return (
-        <div style={appStyle} className="App">
-            <header className="App-header" />
-            <FeedbackForm />
-        </div>
+        <AppContainer>
+            <CenteredAppWithGutters>
+                <FeedbackForm />
+            </CenteredAppWithGutters>
+        </AppContainer>
     )
 }
 
