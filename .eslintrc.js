2d2a48fcfa1e7983e5bc386fe8dab6901aabc236
adameichelkraut <adam.eichelkraut@compozelabs.com>
2022-03-20 11:00:58 -0500

    fix our linter to actually be linting files from root directory down

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
+/* eslint-disable array-bracket-spacing */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    "jest/globals": true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        "jest/globals": true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'jest',
-  ],
-  rules: {
-    "react/jsx-indent": [2, 4],
-    indent: ["error", 4, {
-      "SwitchCase": 1
-    }],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-    "comma-dangle": ["warn", "always"],
-    "no-multiple-empty-lines": ["warn", { "max": 2 }]
-  },
-  ignorePatterns: ["**/*.svg", "**/*.css"],
-};
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: [
+        "react",
+        "jest",
+    ],
+    rules: {
+        "react/jsx-indent": [2, 4, ],
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }, ],
+        "linebreak-style": ["error", "unix", ],
+        quotes: ["error", "double", ],
+        semi: ["error", "never", ],
+        "comma-dangle": ["warn", "always", ],
+        "no-multiple-empty-lines": ["warn", { max: 2, }, ],
+    },
+    ignorePatterns: ["**/*.svg", "**/*.css", ],
+}
